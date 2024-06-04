5f10d35315c2a30128e8af70ea71cdd01cd2d52c
Asyurazhr <Asyuradjola2004@gmail.com>
2024-06-04 15:38:12 +0700

    memperbaiki line 10 dan 11 (menambahkan cout dan cin)

--- enc_temp_folder/dbc85cf2c235949bcff781b4322fbdd/ioFileWithPath.cpp
+++ enc_temp_folder/dbc85cf2c235949bcff781b4322fbdd/ioFileWithPath.cpp
@@ -7,9 +7,6 @@ int main() {
     string baris;
     string NamaFile;
 
-    cout << "masukan nama file :";
-    cin >> NamaFile;
-
     // membuka file dalam mode menulis.
     ofstream outfile;
     // menunjuk ke sebuah nama file
